5653c30d90e6ae7fffc4582d4ed1980cbf64325f
Nataly <ncgarzon88@gmail.com>
2018-07-30 18:16:50 -0400

    nav working

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,7 @@ console.log("Eat More Oreos");
 //Showing Navigation on Air Balloon
 var navItem = document.querySelectorAll("header nav ul a");
 var nav = document.querySelector("nav");
+var ladder = document.querySelector(".ladder");
 
 navItem.forEach(function (element) {
 	element.style.color = "transparent";
@@ -14,11 +15,13 @@ nav.addEventListener("mouseover", function () {
 	navItem.forEach(function (element) {
 		element.style.color = "#fff";
 	});
+	// ladder.style.display = "block";
 });
 
 nav.addEventListener("mouseout", function () {
 	navItem.forEach(function (element) {
 		element.style.color = "transparent";
 	});
+	// ladder.style.display = "none";
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ console.log(`Eat More Oreos`)
 //Showing Navigation on Air Balloon
 var navItem = document.querySelectorAll("header nav ul a");
 var nav = document.querySelector("nav");
+var ladder = document.querySelector(".ladder")
 
 navItem.forEach(function(element){
 		element.style.color = "transparent";	
@@ -12,12 +13,14 @@ nav.addEventListener("mouseover", function(){
 	navItem.forEach(function(element){
 		element.style.color = "#fff";	
 	});
+	// ladder.style.display = "block";
 });
 
 nav.addEventListener("mouseout", function(){
 	navItem.forEach(function(element){
 		element.style.color = "transparent";	
 	});
+	// ladder.style.display = "none";
 });
 
 
