975e82d2678b9c552b97e986261da04e359c6bca
Nataly <ncgarzon88@gmail.com>
2018-07-30 16:53:22 -0400

    working on the nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,24 @@
 "use strict";
 
 console.log("Eat More Oreos");
+
+//Showing Navigation on Air Balloon
+var navItem = document.querySelectorAll("header nav ul a");
+var nav = document.querySelector("nav");
+
+navItem.forEach(function (element) {
+	element.style.color = "transparent";
+});
+
+nav.addEventListener("mouseover", function () {
+	navItem.forEach(function (element) {
+		element.style.color = "#fff";
+	});
+});
+
+nav.addEventListener("mouseout", function () {
+	navItem.forEach(function (element) {
+		element.style.color = "transparent";
+	});
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 console.log(`Eat More Oreos`)
+
+//Showing Navigation on Air Balloon
+var navItem = document.querySelectorAll("header nav ul a");
+var nav = document.querySelector("nav");
+
+navItem.forEach(function(element){
+		element.style.color = "transparent";	
+	});
+
+nav.addEventListener("mouseover", function(){
+	navItem.forEach(function(element){
+		element.style.color = "#fff";	
+	});
+});
+
+nav.addEventListener("mouseout", function(){
+	navItem.forEach(function(element){
+		element.style.color = "transparent";	
+	});
+});
+
+
+
+
+
