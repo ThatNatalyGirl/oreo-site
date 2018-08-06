b377eb0205c23975d762c4af0389e582cf6bb563
Angelica de Wit <angelicadewit@Angelicas-MacBook-Pro.local>
2018-08-06 17:57:02 -0400

    made some changes to scss and js

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -30,8 +30,8 @@ class Tooltip {
 	
 	showTooltip() {
 		console.log('show tooltip')
-		this.$tooltip.style.left = /*this.$triggerElement.getBoundingClientRect().right +*/ 100 + "px";
-		this.$tooltip.style.top = /*this.$triggerElement.getBoundingClientRect().top + */ 205 + "%";
+		this.$tooltip.style.left = this.$triggerElement.getBoundingClientRect().right + window.scrollX - 100 + "px";
+		this.$tooltip.style.top = this.$triggerElement.getBoundingClientRect().top + window.scrollY + 100 + "px";
 		this.$tooltip.style.opacity = 1;
 		// setTimeout(() => {
 		// 	this.$tooltip.style.top = this.$triggerElement.getBoundingClientRect().top - 10 + "px";
