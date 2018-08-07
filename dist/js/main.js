5beed0ff9a2bc63f3db5ad3600a01bccfb6c7d29
Nataly <ncgarzon88@gmail.com>
2018-08-06 23:40:07 -0400

    recipe book totally working :D

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,77 +8,95 @@ var nav = document.querySelector("nav");
 var ladder = document.querySelector(".ladder");
 
 navItem.forEach(function (element) {
-    element.style.color = "transparent";
+	element.style.color = "transparent";
 });
 ladder.style.display = "none";
 
 nav.addEventListener("mouseover", function () {
-    navItem.forEach(function (element) {
-        element.style.color = "#fff";
-    });
-    ladder.style.display = "block";
+	navItem.forEach(function (element) {
+		element.style.color = "#fff";
+	});
+	ladder.style.display = "block";
 });
 
 nav.addEventListener("mouseout", function () {
-    navItem.forEach(function (element) {
-        element.style.color = "transparent";
-    });
-    ladder.style.display = "none";
+	navItem.forEach(function (element) {
+		element.style.color = "transparent";
+	});
+	ladder.style.display = "none";
 });
 
 //oreo flavors showing
 (function () {
-    var controller = new ScrollMagic.Controller({ reverse: true, offset: '0px' });
+	var controller = new ScrollMagic.Controller({ reverse: true, offset: '0px' });
 
-    new ScrollMagic.Scene({ triggerElement: ".videoSect" }).setClassToggle(".logo", "is-smaller") // add class toggle
-    .addTo(controller);
-    new ScrollMagic.Scene({ triggerElement: ".game2Sect" }).setClassToggle(".logo", "is-dunking") // add class toggle
-    .addTo(controller);
+	new ScrollMagic.Scene({ triggerElement: ".videoSect" }).setClassToggle(".logo", "is-smaller") // add class toggle
+	.addTo(controller);
+	new ScrollMagic.Scene({ triggerElement: ".game2Sect" }).setClassToggle(".logo", "is-dunking") // add class toggle
+	.addTo(controller);
 })();
 
-//Flavor Oreo Tooltips
-var tt1 = new TooltipFirst({
-    text: 'Strawberry Shortcake',
-    selector: '#shortcake'
+var tt1 = new Tooltip({
+	text: 'Strawberry Shortcake',
+	selector: '#shortcake'
 });
 
 var tt2 = new Tooltip({
-    text: 'Golden',
-    selector: '#golden'
+	text: 'Golden',
+	selector: '#golden'
 });
 
 var tt3 = new Tooltip({
-    text: 'Peanut Butter',
-    selector: '#pb'
+	text: 'Peanut Butter',
+	selector: '#pb'
 });
 
 var tt4 = new Tooltip({
-    text: 'Red Velvet',
-    selector: '#redvelvet'
+	text: 'Red Velvet',
+	selector: '#redvelvet'
 });
 
 var tt5 = new Tooltip({
-    text: 'Mint Chocolate',
-    selector: '#mint'
+	text: 'Mint Chocolate',
+	selector: '#mint'
 });
 
 var tt6 = new Tooltip({
-    text: 'Birthday Cake',
-    selector: '#bday'
+	text: 'Birthday Cake',
+	selector: '#bday'
 });
 
 var tt7 = new Tooltip({
-    text: 'Pumpkin Spice',
-    selector: '#pspice'
+	text: 'Pumpkin Spice',
+	selector: '#pspice'
 });
 
 var tt8 = new Tooltip({
-    text: 'Classic',
-    selector: '#orig'
+	text: 'Classic',
+	selector: '#orig'
 });
 
 var tt9 = new Tooltip({
-    text: 'Lemon',
-    selector: '#lemon'
+	text: 'Lemon',
+	selector: '#lemon'
+});
+
+//Recipe Book
+
+var leftArrow = document.querySelector(".recipeSect .book .previous");
+var rightArrow = document.querySelector(".recipeSect .book .next");
+var page2 = document.querySelector(".recipeSect .book .page-2");
+var page3 = document.querySelector(".recipeSect .book .page-3");
+
+leftArrow.addEventListener("click", function (event) {
+	page2.style.transform = "rotateY(0deg)";
+	page3.style.transform = "rotateY(180deg)";
+	console.log("right");
+});
+
+rightArrow.addEventListener("click", function (event) {
+	page2.style.transform = "rotateY(180deg)";
+	page3.style.transform = "rotateY(0deg)";
+	console.log("left");
 });
 //# sourceMappingURL=main.js.map
