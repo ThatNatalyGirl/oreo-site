ab9bef8f651aebb4cce5ed2c995281087fda12f5
Nataly <ncgarzon88@gmail.com>
2018-10-17 16:13:19 -0400

    final commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,63 +27,82 @@ nav.addEventListener("mouseout", function () {
 });
 
 //oreo flavors showing
-(function () {
-	var controller = new ScrollMagic.Controller({ reverse: true, offset: '0px' });
+var oreos = document.querySelectorAll(".flavorSect a img");
 
-	new ScrollMagic.Scene({ triggerElement: ".cloud-4" }).setClassToggle(".logo-moving", "not-smaller") // add class toggle
-	.addTo(controller);
-	new ScrollMagic.Scene({ triggerElement: ".videoSect" }).setClassToggle(".logo", "no-z-index") // add class toggle
-	.addTo(controller);
-	new ScrollMagic.Scene({ triggerElement: ".videoSect" }).setClassToggle(".logo-moving", "is-smaller") // add class toggle
-	.addTo(controller);
-	new ScrollMagic.Scene({ triggerElement: ".game2Sect" }).setClassToggle(".logo-moving", "is-dunking") // add class toggle
-	.addTo(controller);
-})();
+oreos.forEach(function (oreo) {
+	var label = oreo.nextSibling.nextSibling;
+	oreo.addEventListener("mouseover", function () {
+		label.style.opacity = "1";
+		console.log("I'm an oreo");
+	});
 
-var tt1 = new TooltipFirst({
-	text: 'Strawberry Shortcake',
-	selector: '#shortcake'
+	oreo.addEventListener("mouseout", function () {
+		label.style.opacity = "0";
+	});
 });
 
-var tt2 = new Tooltip({
-	text: 'Golden',
-	selector: '#golden'
-});
+// (function () {
+// 	var controller = new ScrollMagic.Controller({reverse:true, offset: '0px'});
 
-var tt3 = new Tooltip({
-	text: 'Peanut Butter',
-	selector: '#pb'
-});
+// 	new ScrollMagic.Scene({triggerElement: ".cloud-4"})
+// 		.setClassToggle(".logo-moving", ".not-smaller") // add class toggle
+//         .addTo(controller);
+//         new ScrollMagic.Scene({triggerElement: ".videoSect"})
+// 		.setClassToggle(".logo", "no-z-index") // add class toggle
+// 		.addTo(controller)
+//         new ScrollMagic.Scene({triggerElement: ".videoSect"})
+// 		.setClassToggle(".logo-moving", "is-smaller") // add class toggle
+// 		.addTo(controller);
+// 	new ScrollMagic.Scene({triggerElement: ".game2Sect"})
+// 		.setClassToggle(".logo-moving", "is-dunking") // add class toggle
+// 		.addTo(controller);
+// })();
 
-var tt4 = new Tooltip({
-	text: 'Red Velvet',
-	selector: '#redvelvet'
-});
+// let tt1 = new TooltipFirst({
+// 	text: 'Strawberry Shortcake',
+// 	selector: '#shortcake',
+// })
 
-var tt5 = new Tooltip({
-	text: 'Mint Chocolate',
-	selector: '#mint'
-});
+// let tt2 = new Tooltip({
+// 	text: 'Golden',
+// 	selector: '#golden',
+// })
 
-var tt6 = new Tooltip({
-	text: 'Birthday Cake',
-	selector: '#bday'
-});
+// let tt3 = new Tooltip({
+// 	text: 'Peanut Butter',
+// 	selector: '#pb',
+// })
 
-var tt7 = new Tooltip({
-	text: 'Pumpkin Spice',
-	selector: '#pspice'
-});
+// let tt4 = new Tooltip({
+// 	text: 'Red Velvet',
+// 	selector: '#redvelvet',
+// })
 
-var tt8 = new Tooltip({
-	text: 'Classic',
-	selector: '#orig'
-});
+// let tt5 = new Tooltip({
+// 	text: 'Mint Chocolate',
+// 	selector: '#mint',
+// })
 
-var tt9 = new Tooltip({
-	text: 'Lemon',
-	selector: '#lemon'
-});
+// let tt6 = new Tooltip({
+// 	text: 'Birthday Cake',
+// 	selector: '#bday',
+// })
+
+// let tt7 = new Tooltip({
+// 	text: 'Pumpkin Spice',
+// 	selector: '#pspice',
+// })
+
+// let tt8 = new Tooltip({
+// 	text: 'Classic',
+// 	selector: '#orig',
+// })
+
+// let tt9 = new Tooltip({
+// 	text: 'Lemon',
+// 	selector: '#lemon',
+// })
+
 
 //Recipe Book
 
--- src/js/main.js
+++ src/js/main.js
@@ -27,67 +27,83 @@ nav.addEventListener("mouseout", function(){
 
 
 //oreo flavors showing
-(function () {
-	var controller = new ScrollMagic.Controller({reverse:true, offset: '0px'});
+var oreos = document.querySelectorAll(".flavorSect a img")
+
+oreos.forEach(function(oreo){
+	var label = oreo.nextSibling.nextSibling
+	oreo.addEventListener("mouseover", function(){
+		label.style.opacity = "1"
+		console.log("I'm an oreo")
+	})
+
+	oreo.addEventListener("mouseout", function(){
+		label.style.opacity = "0"
+	})
+})
+
+
+
+// (function () {
+// 	var controller = new ScrollMagic.Controller({reverse:true, offset: '0px'});
   
-	new ScrollMagic.Scene({triggerElement: ".cloud-4"})
-		.setClassToggle(".logo-moving", "not-smaller") // add class toggle
-        .addTo(controller);
-        new ScrollMagic.Scene({triggerElement: ".videoSect"})
-		.setClassToggle(".logo", "no-z-index") // add class toggle
-		.addTo(controller)
-        new ScrollMagic.Scene({triggerElement: ".videoSect"})
-		.setClassToggle(".logo-moving", "is-smaller") // add class toggle
-		.addTo(controller);
-	new ScrollMagic.Scene({triggerElement: ".game2Sect"})
-		.setClassToggle(".logo-moving", "is-dunking") // add class toggle
-		.addTo(controller);
-})();
+// 	new ScrollMagic.Scene({triggerElement: ".cloud-4"})
+// 		.setClassToggle(".logo-moving", ".not-smaller") // add class toggle
+//         .addTo(controller);
+//         new ScrollMagic.Scene({triggerElement: ".videoSect"})
+// 		.setClassToggle(".logo", "no-z-index") // add class toggle
+// 		.addTo(controller)
+//         new ScrollMagic.Scene({triggerElement: ".videoSect"})
+// 		.setClassToggle(".logo-moving", "is-smaller") // add class toggle
+// 		.addTo(controller);
+// 	new ScrollMagic.Scene({triggerElement: ".game2Sect"})
+// 		.setClassToggle(".logo-moving", "is-dunking") // add class toggle
+// 		.addTo(controller);
+// })();
 
-let tt1 = new TooltipFirst({
-	text: 'Strawberry Shortcake',
-	selector: '#shortcake',
-})
+// let tt1 = new TooltipFirst({
+// 	text: 'Strawberry Shortcake',
+// 	selector: '#shortcake',
+// })
 
-let tt2 = new Tooltip({
-	text: 'Golden',
-	selector: '#golden',
-})
+// let tt2 = new Tooltip({
+// 	text: 'Golden',
+// 	selector: '#golden',
+// })
 
-let tt3 = new Tooltip({
-	text: 'Peanut Butter',
-	selector: '#pb',
-})
+// let tt3 = new Tooltip({
+// 	text: 'Peanut Butter',
+// 	selector: '#pb',
+// })
 
-let tt4 = new Tooltip({
-	text: 'Red Velvet',
-	selector: '#redvelvet',
-})
+// let tt4 = new Tooltip({
+// 	text: 'Red Velvet',
+// 	selector: '#redvelvet',
+// })
 
-let tt5 = new Tooltip({
-	text: 'Mint Chocolate',
-	selector: '#mint',
-})
+// let tt5 = new Tooltip({
+// 	text: 'Mint Chocolate',
+// 	selector: '#mint',
+// })
 
-let tt6 = new Tooltip({
-	text: 'Birthday Cake',
-	selector: '#bday',
-})
+// let tt6 = new Tooltip({
+// 	text: 'Birthday Cake',
+// 	selector: '#bday',
+// })
 
-let tt7 = new Tooltip({
-	text: 'Pumpkin Spice',
-	selector: '#pspice',
-})
+// let tt7 = new Tooltip({
+// 	text: 'Pumpkin Spice',
+// 	selector: '#pspice',
+// })
 
-let tt8 = new Tooltip({
-	text: 'Classic',
-	selector: '#orig',
-})
+// let tt8 = new Tooltip({
+// 	text: 'Classic',
+// 	selector: '#orig',
+// })
 
-let tt9 = new Tooltip({
-	text: 'Lemon',
-	selector: '#lemon',
-})
+// let tt9 = new Tooltip({
+// 	text: 'Lemon',
+// 	selector: '#lemon',
+// })
 
 
 //Recipe Book
